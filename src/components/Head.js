97ca9ebe047ebec8619b0cde43b09b8ccf9b7101
Vishal Kumar <iamvishukr@gmail.com>
2024-05-04 22:45:06 +0530

    creating header

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const Head = () => {
   return (
-    <div className='m-4'>Head</div>
-  )
-}
+    <div className="flex  shadow-lg p-3">
+      <div className="flex gap-6">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/800px-Hamburger_icon.svg.png"
+          alt="menu"
+          className="h-8"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/0/09/YouTube_full-color_icon_%282017%29.svg"
+          alt="youtube"
+          className="h-8 "
+        />
+      </div>
+      <div className="m-auto ">
+        <input
+          type="text"
+          placeholder="Search"
+          id=""
+          className="border border-black w-[450px] px-3 py-2 rounded-l-3xl"
+        />
+        <button className="border border-black px-3 py-2 rounded-r-3xl border-l-0">Search</button>
+      </div>
+      <div className="-my-4">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMfnEpNt3m7Gm_5RQO54mtgpmejdTziTlDkDD5IxnB8w&s" alt="user" className="h-16 " />
+      </div>
+    </div>
+  );
+};
 
-export default Head
+export default Head;
