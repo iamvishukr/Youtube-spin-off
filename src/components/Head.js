6f37cc085b2d4678f973474927a0bac6ab31ffc6
Vishal Kumar <iamvishukr@gmail.com>
2024-05-14 17:09:43 +0530

    created searchSlice to store search cache (It will store the cache which will result in making less api call if searching the same thing || will make our app faster and more efficient)

--- src/components/Head.js
+++ src/components/Head.js
@@ -3,16 +3,26 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { FaYoutube } from "react-icons/fa";
 import { FaRegUserCircle } from "react-icons/fa";
 import { toggleMenu } from "../utils/appSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { IoMdSearch } from "react-icons/io";
 
 import { YOUTUBE_SEARCH_API } from "../utils/constant";
+import { cacheResults } from "../utils/searchSlice";
 
 const Head = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const searchCache = useSelector(store => store.search);
   useEffect(() => {
-    const timer = setTimeout(() => getSearchSuggestion(), 200);
+
+    const timer = setTimeout(() =>{
+      if(searchCache[searchQuery]){
+        setSuggestions(searchCache[searchQuery]);
+      }else{
+       getSearchSuggestion();
+        } 
+      },200);
 
     return () => {
       clearTimeout(timer);
@@ -23,6 +33,9 @@ const Head = () => {
     const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
     const json = await data.json();
     setSuggestions(json[1]);
+    dispatch(cacheResults({
+      [searchQuery] : json[1],
+    }))
   };
 
   const dispatch = useDispatch();
@@ -44,23 +57,29 @@ const Head = () => {
         <input
           type="text"
           placeholder="Search"
-          className="border border-gray-400 w-[450px] px-3 py-2 rounded-l-3xl"
+          className="border border-gray-400 w-[600px] px-3 py-2 rounded-l-3xl"
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
+          onFocus={(e) => setShowSuggestions(true)}
+          onBlur={(e) => setShowSuggestions(false)}
+        
+          
         />
         <button className="border border-gray-400 px-4 py-2  rounded-r-3xl  border-l-0 bg-gray-100 text-gray-600 ">
           Search
         </button>
-        <div className="fixed py-2 px-0 bg-white w-[27rem] ml-4 shadow-lg rounded-lg border-gray-200">
-          <ul>
+       {showSuggestions && <div className="absolute py-2 px-0 bg-white w-[37rem] ml-1 shadow-lg rounded-lg border-gray-200" >
+          <ul >
             {suggestions.map((suggestion) => (
-              <li className="flex hover:bg-slate-100  gap-2 px-3">
+              <li className="flex hover:bg-slate-100  gap-2 px-3 text-lg" key={suggestion}>
                 <IoMdSearch className="mt-1 " />
                 {suggestion}
               </li>
             ))}
           </ul>
         </div>
+        
+        }
       </div>
 
       <div>
